823a7ec4a16bf7ff2953901eec2f29f813f748df
Navneet Kumar <navneetsinghpanwar90@gmail.com>
2023-04-13 14:20:20 +0530

    added, screenshot, readme, responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = ({ size }) => {
     return (
         <nav>
             <div className="nav-bar">
-                <Link to='/' style={{ textDecoration: 'none', color: "black" }}><p className="cart-button">Food Products</p></Link>
+                <Link to='/' style={{ textDecoration: 'none', color: "black" }}><p className="cart-button">Products</p></Link>
                 <h1 className="app-name">Shopping Cart</h1>
                 <div className="cart-size">
                     <Link to='/cart' style={{ textDecoration: 'none', color: "black" }}><p className="cart-button">Cart</p></Link>
